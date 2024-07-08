8a43ad14aaf54a1ab890f789c9b749324c92e827
sarah <sarahrefaatsss@gmail.com>
2024-07-08 23:37:57 +0200

    push the application

---
+++ footer.php
@@ -0,0 +1,9 @@
+</main>
+
+<footer>
+    <p>&copy; 2024 Lions Web App. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<? php include 'header.php'; ?>
+
+<div class="content">
+    <h1>Welcome to the World of Lions</h1>
+    <p>Lions are majestic creatures known for their strength and beauty...</p>
+    <img src="images/lion.jpg" alt="Lion">
+    <p>Learn more about lions and their habitats...</p>
+</div>
+
+<?php include 'footer.php'; ?>
+
